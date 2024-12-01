a40eb21b6b08e9a939daf4066ccdb6a393758105
Jeff Bolduan <jbolduan@umn.edu>
2024-11-30 19:11:22 -0600

    small layout change to statement pages

--- resources/views/statements/show.blade.php
+++ resources/views/statements/show.blade.php
@@ -2,24 +2,26 @@
 
 @section('content')
 
-<div class="h1 text-center">{{ $statement->title }}</div>
+<div class="h1 text-center m-2">{{ $statement->title }}</div>
+<!-- <hr class="border-4" style="color: #efa00b;"> -->
+<div class="table-responsive mt-4">
 <table class="table">
     <tbody>
-        <tr>
-            <td><b>Statement Description:</b></td>
-            <td>{{ $statement->description }}</td>
+        <tr class="p-4 border-top">
+            <td class="p-4"><span class="fw-bold fs-5 align-middle">Statement Description:</span></td>
+            <td class="align-middle p-4">{{ $statement->description }}</td>
+        </tr>
+        <tr class="p-4">
+            <td class="p-4"><span class="fw-bold fs-5 align-middle">Statement Status:</s></td>
+            <td class="align-middle p-4">{{ $statement->status->name }}</td>
         </tr>
         <tr>
-            <td><b>Statement Status:</b></td>
-            <td>{{ $statement->status->name }}</td>
+            <td class="p-4"><span class="fw-bold fs-5 align-middle">Statement Status Details:</span></td>
+            <td class="align-middle p-4">{{ $statement->status_details }}</td>
         </tr>
-        <tr>
-            <td><b>Statement Status Details:</b></td>
-            <td>{{ $statement->status_details }}</td>
-        </tr>
-        <tr>
-            <td><b>Categories:</b></td>
-            <td>
+        <tr class="p-4">
+            <td class="p-4"><span class="fw-bold fs-5 align-middle">Categories:</span></td>
+            <td class="align-middle p-4">
                 @if($statement->category->count() > 0)
                 @foreach($statement->category as $category)
                 <span class="badge rounded-pill category-pill-{{ $category->id }}">{{ $category->name }}</span>
@@ -29,9 +31,9 @@
                 @endif
             </td>
         </tr>
-        <tr>
-            <td><b>Statement URLs:</b></td>
-            <td>
+        <tr class="p-4">
+            <td class="p-4"><span class="fw-bold fs-5 align-middle">Statement URLs:</span></td>
+            <td class="align-middle p-4">
                 <?php
                 $urls = array_filter(preg_split("/\r\n|\n|\r/", $statement->urls));
                 $urlsCount = count($urls);
@@ -49,5 +51,6 @@
         </tr>
     </tbody>
 </table>
+</div>
 
 @endsection
